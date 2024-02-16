3f660c7c2b596eb2938ea6a60945d432e556c825
rizalfirmansyah11 <140318211+rizalfirmansyah11@users.noreply.github.com>
2024-02-16 23:53:50 +0700

    Update select.php

--- kategori/select.php
+++ kategori/select.php
@@ -1,8 +1,40 @@
+<div style="margin:auto; width: 900px;">
+
 <?php
 
     require_once "../function.php";
 
-    $sql = "SELECT * FROM tblkategori";
+    $sql = "SELECT idkategori FROM tblkategori";
+    $result = mysqli_query($koneksi,$sql);
+
+    $jumlahdata = mysqli_num_rows($result);
+
+   
+
+    $mulai = 3;
+    $banyak = 3;
+
+    $halaman = ceil($jumlahdata / $banyak);
+
+   for ($i=1; $i <= $halaman ; $i++) { 
+    echo '<a href="?p='.$i.'">'.$i.'</a>';
+    echo '&nbsp &nbsp &nbsp';
+   }
+        
+    
+    echo '<br> <br>';
+
+    if (isset($_GET['p'])) {
+       $p=$_GET['p'];
+       $mulai = ($p * $banyak)- $banyak;
+        // = (2*3) - 3
+    }else {
+        $mulai =0;
+    }
+
+
+
+    $sql = "SELECT * FROM tblkategori LIMIT $mulai,$banyak";
 
    
     $result = mysqli_query($koneksi,$sql);
@@ -24,7 +56,7 @@
     
     
     ';
-    $no=1;
+    $no=$mulai+1;
 
     if ($jumlah > 0 ) {
         while ($row = mysqli_fetch_assoc($result)) {
@@ -39,5 +71,8 @@
 ?>
 
 
+
+
+</div>
     
    
