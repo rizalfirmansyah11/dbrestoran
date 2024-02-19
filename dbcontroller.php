79c17c4b6e8264e537590b54539820db051bc53a
rizalfirmansyah11 <140318211+rizalfirmansyah11@users.noreply.github.com>
2024-02-19 22:55:31 +0700

    project aplikasi restoran

--- dbcontroller.php
+++ dbcontroller.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
     class DB{
 
@@ -10,61 +10,49 @@
 
         public function __construct()
         {
-            $this->koneksi = $this->koneksiDB();    
+           $this->koneksi=$this->koneksiDB(); 
         }
-
-
-       public  function koneksiDB()
+        
+        public function koneksiDB()
         {
-            $koneksi = mysqli_connect($this->host,$this->user,$this->password,$this->database);    
+            $koneksi=mysqli_connect($this->host,$this->user,$this->password,$this->database);
             return $koneksi;
         }
 
         public function getALL($sql)
         {
-            $result = mysqli_query($this-> koneksi, $sql);
-            while($row=mysqli_fetch_assoc($result)){
+            $result =mysqli_query($this->koneksi,$sql);
+            while ($row=mysqli_fetch_assoc($result)) {
                 $data[]=$row;
             }
-            return $data;
+            if (!empty($data)) {
+                return $data;
+            }
         }
-
         public function getITEM($sql)
         {
-            $result = mysqli_query($this->koneksi, $sql);
-            $row = mysqli_fetch_assoc($result);
+            $result =mysqli_query($this->koneksi,$sql);
+            $row=mysqli_fetch_assoc($result);
             return $row;
         }
-
-        public function rowCOUNT($sql) 
+        public function rowCOUNT($sql)
         {
-            $result = mysqli_query($this->koneksi,$sql);
-            $count = mysqli_num_rows($result);
-            
+            $result =mysqli_query($this->koneksi,$sql);
+            $count=mysqli_num_rows($result);
             return $count;
         }
-
-        public function runSQL($sql) 
+        public function runSQL($sql)
         {
-            $result = mysqli_query($this->koneksi,$sql);
+            $result =mysqli_query($this->koneksi,$sql);
+            
         }
-
         public function pesan($text="")
         {
-            echo $text;
+            echo $text;    
         }
     }
-
-    $db = new DB;
-
-    
-    // $db->pesan("berhasil");
    
 
-    //var_dump($row);
-
-
-    // foreach ($row as $key){
-    //     echo $key['kategori'].'<br>';
-    // }
 ?>
+
+
--- kategori/select.php
+++ kategori/select.php
@@ -1,61 +1,58 @@
-<?php
-$jumlahdata = $db->rowCOUNT("SELECT idkategori FROM tblkategori");
-$banyak = 3;
+<?php 
+    $jumlahdata=$db->rowCOUNT("SELECT idkategori FROM tblkategori");
 
-$halaman = ceil($jumlahdata / $banyak);
+    $banyak=3;
 
+    $halaman= ceil($jumlahdata/$banyak);
 
-if (isset($_GET['p'])) {
-    $p=$_GET['p'];
-    $mulai = ($p * $banyak)- $banyak;
+    if (isset($_GET['p'])) {
+        $p=$_GET['p'];
+        $mulai=($p*$banyak)-$banyak;
+    }else {
+        $mulai=0;
+    }
+
+    $sql="SELECT * FROM tblkategori ORDER BY kategori ASC LIMIT $mulai,$banyak";
+    $row=$db->getALL($sql);
     
- }else {
-     $mulai =0;
- }
+    $no=1+$mulai;
 
 
-  $sql= "SELECT * FROM tblkategori ORDER BY kategori ASC LIMIT $mulai,$banyak";
-  $row= $db->getALL($sql);
-
- $no=1+$mulai;
 ?>
 
-<h1> kategori </h1>
+<div class="float-left mr-4">
+    <a class="btn btn-primary" href="?f=kategori&m=insert" role="button">TAMBAH DATA</a>
+</div>
+
+<h3>Kategori</h3>
 <table class="table table-bordered w-50">
+
     <thead>
         <tr>
             <th>No</th>
             <th>Kategori</th>
             <th>Delete</th>
             <th>Update</th>
+
         </tr>
     </thead>
-    <thead>
     <tbody>
-        <?php $NO = 1; foreach($row as $r): ?>
+        <?php foreach($row as $r): ?>
+            <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $r['kategori'] ?></td>
+                <td><a href="?f=kategori&m=delete&id=<?php echo $r['idkategori'] ?>">Delete</a></td>
+                <td><a href="?f=kategori&m=update&id=<?php echo $r['idkategori'] ?>">Update</a></td>
 
-    <tr>
-    <td><?php echo $NO++ ?></td>
-        <td><?php echo $r['kategori'] ?></td>
-        <td><?php echo $r['idkategori'] ?></td>
-        <td><?php echo $r['idkategori'] ?></td>
-    </tr>
-       <?php endforeach ?>
-
+            </tr>
+        <?php endforeach ?>
     </tbody>
-
-    </thead>
-
-
-
 </table>
+<?php 
 
-<?php
-
-for ($i=1; $i <= $halaman ; $i++) { 
-    echo '<a href="?f=kategori&m=select&p='.$i.'">'.$i.'</a>';
-    echo '&nbsp &nbsp &nbsp';
-   }
-
+    for ($i=1; $i <=$halaman ; $i++) { 
+        echo '<a href="?f=kategori&m=select&p='.$i.'">'.$i.'</a>';
+        echo '&nbsp &nbsp &nbsp';
+    }
 
 ?>
