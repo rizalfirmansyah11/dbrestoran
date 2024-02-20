025ebb49379d036ce0bac5f6330c6c51e31f5db8
rizalfirmansyah11 <140318211+rizalfirmansyah11@users.noreply.github.com>
2024-02-20 22:13:26 +0700

    update user aplikasi

--- admin/login.php
+++ admin/login.php
@@ -1,64 +1,82 @@
+<?php 
 
-<?php
-    session_start();
-    require_once('../dbcontroller.php');
-    $db = new DB;
+session_start();
+require_once"../dbcontroller.php";
+$db= new DB;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
     <title>Login Restoran</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
 </head>
 <body>
-    <div class="container">
+    <div class="class">
+
         <div class="row">
+
             <div class="col-4 mx-auto mt-4">
-                <h3>LOGIN RESTORAN</h3>
-            <form action="" method="post">
-                <div class="mb-3 w-50" >
-                    <label for="" class="form-label">Email : </label>
-                    <input type="email" name="email" required placeholder="email" class="form-control">
+                <div class="form-group">
+
+                    <form action="" method="post">
+                        <div>
+
+                            <h3>LOGIN RESTORAN</h3>
+
+                        </div>
+                        <div class="form-group">
+                            <label for="">EMAIL</label>
+                            <input type="email" name="email" required  class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">PASSWORD</label>
+                            <input type="password" name="password" required  class="form-control">
+                        </div>
+                        <div>
+
+                            <input type="submit" name="login" value="LOGIN" class="btn btn-primary">
+
+                        </div>
+                    </form>
                 </div>
-                <div class="mb-3 w-50">
-                    <label for="" class="form-label">Password : </label>
-                    <input type="password" name="password" required placeholder="password" class="form-control">
-                </div>
-                <div class="float-start">
-                    <input type="submit" name="login" value="login" class="btn btn-primary">
-                </div>
-            </form>
+                <?php 
+
+                    if (isset($_POST['simpan'])) {
+                        $kategori=$_POST['kategori'];
+                        $sql="INSERT INTO tblkategori VALUES('','$kategori')";
+                        
+                        $db->runSQL($sql);
+                        header("location:?f=kategori&m=select");
+                    }
+
+                ?>
             </div>
+
         </div>
+
     </div>
 </body>
 </html>
-<?php
+<?php 
 
     if (isset($_POST['login'])) {
-        $email = $_POST['email'];
-        $password = hash('sha256',$_POST['password']);
-
-        $sql = "SELECT * FROM tbluser WHERE email='$email' AND password='$password' ";
-
-        $count = $db->rowCount($sql);
-
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $sql="SELECT * FROM tbluser WHERE email='$email' AND $password";
+        $count=$db->rowCOUNT($sql);
         if ($count == 0) {
-            echo "<center><h3>email atau password salah</h3></center>";
-        }
-        else {
-            $sql = "SELECT * FROM tbluser WHERE email='$email' AND password='$password' ";
-            $row = $db->getITEM($sql);
-
+            echo"<h3>Email atau Password salah</h3>";
+        }else {
+            $sql="SELECT * FROM tbluser WHERE email='$email' AND $password";
+            $row=$db->getITEM($sql);
             $_SESSION['user']=$row['email'];
             $_SESSION['level']=$row['level'];
-            $_SESSION['iduser']=$row['iduser'];
-
             header("location:index.php");
         }
+        echo $count;
     }
 
 ?>
