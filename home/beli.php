a6240a5d282def8686f75898bb02c1235207ff9c
rizalfirmansyah11 <140318211+rizalfirmansyah11@users.noreply.github.com>
2024-02-22 22:41:39 +0700

    mengosongkan session aplikasi

--- home/beli.php
+++ home/beli.php
@@ -5,6 +5,19 @@ if (isset($_GET['hapus'])) {
     unset($_SESSION['_' . $id]);
 }
 
+if (isset($_GET['tambah'])) {
+    $id = $_GET['tambah'];
+    $_SESSION['_' . $id]++;
+}
+
+if (isset($_GET['kurang'])) {
+    $id = $_GET['kurang'];
+    $_SESSION['_' . $id]--;
+
+    if ($_SESSION['_' . $id] == 0) {
+        unset($_SESSION['_' . $id]);
+    }
+}
 
 if (!isset($_SESSION['pelanggan'])) {
     header("location:?f=home&m=login");
@@ -17,6 +30,7 @@ if (!isset($_SESSION['pelanggan'])) {
         keranjang();
     }
 }
+
 
 
 
@@ -33,9 +47,13 @@ function keranjang()
 {
     global $db;
 
+    $total = 0;
+
+    global $total;
+
     echo '
 
-    <table class="table table-bordered w-50">
+    <table class="table table-bordered w-70">
     
     <tr>
         <th>No</th>
@@ -46,7 +64,6 @@ function keranjang()
     </tr>
     
     ';
-
 
     foreach ($_SESSION as $key => $value) {
         if ($key <> 'pelanggan' && $key <> 'idpelanggan') {
@@ -60,12 +77,32 @@ function keranjang()
                 echo '<tr>';
                 echo '<td>' . $r['menu'] . '</td>';
                 echo '<td>' . $r['harga'] . '</td>';
-                echo '<td>' . $value . '</td>';
+                echo '<td><a href="?f=home&m=beli&tambah=' . $r['idmenu'] . '"> [+] </a> &nbsp &nbsp ' . $value . '&nbsp &nbsp <a href="?f=home&m=beli&kurang=' . $r['idmenu'] . '"> [-] </a> </td>';
                 echo '<td>' . $r['harga'] * $value . '</td>';
-                echo '<td> <a href="?f=home&m=beli&hapus=' . $r['idmenu'] . '">Hapus</a> </td>';
+                echo '<td> <a href="?f=home&m=beli&hapus=' . $r['idmenu'] . '"> Hapus </a> </td>';
                 echo '</tr>';
+                $total = $total + ($value * $r['harga']);
             }
         }
     }
+
+    echo '<tr>
+        <td colspan=4><h3>GRAND TOTAL : </h3></td>
+        <td><h3>' . $total . '</h3></td>
+    </tr>';
+
     echo '</table>';
 }
+
+?>
+
+<?php
+if (!empty($total)) {
+
+?>
+
+    <a class="btn btn-primary" href="?f=home&m=checkout&total=<?php echo $total ?>" role="button">CHECK OUT</a>
+
+<?php
+}
+?>
